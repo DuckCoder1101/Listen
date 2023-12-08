73f7d5ff83c3cc2351dd16dae4b936e8d0f1e6dd
DuckCoder1101 <crisfavaedu@gmail.com>
2023-12-08 16:25:13 -0300

    ToListen Commit - 08/12/23

--- build/utils/updater.js
+++ build/utils/updater.js
@@ -8,6 +8,9 @@ const electron_log_1 = __importDefault(require("electron-log"));
 function CheckForUpdates() {
     electron_log_1.default.transports.file.level = "debug";
     electron_updater_1.autoUpdater.logger = electron_log_1.default;
+    electron_updater_1.autoUpdater.autoDownload = true;
+    electron_updater_1.autoUpdater.autoInstallOnAppQuit = true;
+    electron_updater_1.autoUpdater.autoRunAppAfterInstall = true;
     electron_updater_1.autoUpdater.checkForUpdatesAndNotify({
         title: "Atualização do ToListen!",
         body: "O ToListen irá reiniciar para atualização em breve!."
--- src/utils/updater.ts
+++ src/utils/updater.ts
@@ -5,6 +5,10 @@ export default function CheckForUpdates() {
     electronLog.transports.file.level = "debug";
     autoUpdater.logger = electronLog;
 
+    autoUpdater.autoDownload = true;
+    autoUpdater.autoInstallOnAppQuit = true;
+    autoUpdater.autoRunAppAfterInstall = true;
+
     autoUpdater.checkForUpdatesAndNotify({
         title: "Atualização do ToListen!",
         body: "O ToListen irá reiniciar para atualização em breve!."
